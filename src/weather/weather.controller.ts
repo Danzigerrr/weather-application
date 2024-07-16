41bdbba24f7c3507d41c216ca43ae30bb94e332e
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-07-16 18:47:42 +0200

    add tests to the weather application

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -6,10 +6,10 @@ export class WeatherController {
     constructor(private readonly weatherService: WeatherService) {}
 
     @Get()
-    getWeather(
-        @Query('lat') lat: number,
-        @Query('lon') lon: number,
-    ) {
+    async getWeather(@Query('lat') lat: number, @Query('lon') lon: number) {
+        if (isNaN(lat) || isNaN(lon)) {
+            throw new Error('Invalid latitude or longitude');
+        }
         return this.weatherService.getWeatherData(lat, lon);
     }
 }
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -10,6 +10,9 @@ export class WeatherService {
     constructor(private httpService: HttpService) {}
 
     getWeatherData(lat: number, lon: number): Observable<any> {
+        if (isNaN(lat) || isNaN(lon)) {
+            throw new Error('Invalid latitude or longitude');
+        }
         const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
         return this.httpService.get(url).pipe(
             map(response => response.data),
